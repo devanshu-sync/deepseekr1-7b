7d274487a9ee39d5a1feaa713ad091dc7d3c9614
devanshu-sync <devanshu@syncmedia.io>
2025-07-31 11:17:34 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,11 +1,21 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BitsAndBytesConfig
 
 model_name = "google/gemma-3-12b-it"
-local_path = "./deepseek_model"
+local_path = "./gemma-3-12b-it-model"
 
-# Download and save locally
+# BitsAndBytes config for 8-bit
+bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+
+# Download tokenizer and save
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.save_pretrained(local_path)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+# Download model in 8-bit quantization
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    trust_remote_code=True,
+    device_map="auto",
+    quantization_config=bnb_config
+)
 model.save_pretrained(local_path)
